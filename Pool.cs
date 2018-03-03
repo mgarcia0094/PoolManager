8ec6fe1b1c133d59e7441d8eca66c57759a019e0
Miguel Ángel García Gandía <mgarcia0094@gmail.com>
2018-03-03 03:36:53 +0100

    PoolManager for Unity3D

---
+++ Pool.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PM {
+
+	public class Pool<T> where T : MonoBehaviour {
+		
+		private Stack<T> stack;
+		public delegate T CreateObjectDelegate();
+		public CreateObjectDelegate m_buildMethod;
+		
+		//build constructor
+		public Pool(string namePool, int size, CreateObjectDelegate buildMethod) {
+			stack = new Stack<T>();
+			m_buildMethod = buildMethod;
+			for (int i = 0; i < size; i++) {
+				T objectPoolObj = buildMethod();
+				Store(objectPoolObj);
+			}
+			PoolManager.Instance.AddPool(namePool, this);
+		}
+
+		//Get element
+		public T Get(bool create=true) {
+			if (stack.Count == 0) {
+				if (create) 
+					return m_buildMethod();
+            	else return null;
+			} else return stack.Pop();
+		}
+
+		//Save element
+		public void Store(T obj) {
+			stack.Push(obj);
+		}
+
+	}
+}
+
---
+++ PoolManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PM {
+
+	//SINGLETON
+	public class PoolManager {
+	
+		private static PoolManager instance = null;
+		private PoolManager() {}
+		
+		private static Dictionary<string, System.Object> pools = new Dictionary<string,System.Object>();
+
+		//AddPool key (name), value (pool)
+		public void AddPool(string name, System.Object p) {
+			pools.Add(name, p);
+		}
+
+		//RemovePool by key
+		public void RemovePool(string namePool) {
+			pools.Remove(namePool);
+		}
+
+		//get pool by key, return null if not exist
+		public System.Object GetPool(string namePool) {
+			return pools[namePool];
+		}
+
+		//SINGLETON
+		public static PoolManager Instance {
+			get {
+				if (instance == null) instance = new PoolManager();
+				return instance;
+			}
+		}
+
+	}
+}
